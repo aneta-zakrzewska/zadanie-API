c2ca08b2f5b571545e08163bd002108d91ba857e
aneta-zakrzewska <100867524+aneta-zakrzewska@users.noreply.github.com>
2022-11-19 20:50:10 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector("button");
+const imgDiv = document.querySelector(".images");
+const URL = "https://picsum.photos/v2/list?limit=9";
+
+function showImages() {
+    fetch(URL)
+		.then(res => res.json())
+        .then(data => {
+    data.map(image => {
+        const img = document.createElement("img");
+        imgDiv.appendChild(img);
+        img.setAttribute("src", image.download_url);
+    })
+})
+		.catch(err => console.error(err));
+}
+
+btn.addEventListener("click", () => {
+	btn.style.display = "none";
+    imgDiv.style.display = "visible";
+    showImages()
+});
+
+
