a16decc6c8771f3127054273109cd1b6402b0a68
aneta-zakrzewska <100867524+aneta-zakrzewska@users.noreply.github.com>
2023-01-04 21:23:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,21 +4,21 @@ const URL = "https://picsum.photos/v2/list?limit=9";
 
 function showImages() {
     fetch(URL)
-		.then(res => res.json())
+	.then(res => res.json())
         .then(data => {
-    data.map(image => {
-        const img = document.createElement("img");
-        imgDiv.appendChild(img);
-        img.setAttribute("src", image.download_url);
+   		data.map(image => {
+        	const img = document.createElement("img");
+       		imgDiv.appendChild(img);
+       		img.setAttribute("src", image.download_url);
     })
 })
-		.catch(err => console.error(err));
+	.catch(err => console.error(err));
 }
 
 btn.addEventListener("click", () => {
 	btn.style.display = "none";
-    imgDiv.style.display = "visible";
-    showImages()
+    	imgDiv.style.display = "visible";
+   	showImages()
 });
 
 
